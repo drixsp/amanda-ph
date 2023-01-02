120877e0076a13ec334c8d5f6d5760da83bfb4c2
Drix Ponteres <drix@bposeats.com>
2023-01-02 18:43:16 +0800

    Add top navigation with overlay menu
    This commit will add a navbar to the landing page. Moreover, it will also add a top layer menu that will be shown
    if it is toggled.

--- resources/index.js
+++ resources/index.js
@@ -2,3 +2,15 @@ function scrollFunction(id) {
     var access = document.getElementById(id);
     access.scrollIntoView();
 }
+
+function toggleMenu() {
+    let menuBtn = document.getElementById('menu');
+    var menuOverlay = document.getElementById('menu-overlay');
+    if(menuBtn.className.includes('menu')) {
+        menuBtn.className = "mdi mdi-close";
+        menuOverlay.style.visibility = 'visible';
+    } else {
+        menuBtn.className = "mdi mdi-menu";
+        menuOverlay.style.visibility = 'hidden';
+    }
+}
